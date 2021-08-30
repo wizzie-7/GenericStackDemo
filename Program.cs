ed71550530cb97ad3ab1bc4d70cc7321a1ccc1d8
Vivek Jadhav <jadhavvivek619@gmail.com>
2021-08-30 18:31:16 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GenericStackDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
